e39610f7ec91f9c269331035fa39201803a4366d
peter <pangercheng@163.com>
2019-03-10 09:25:30 +0800

    update

--- eureka-client/src/main/java/com/cloud/study/cqs/eurekaclient/web/IndexController.java
+++ eureka-client/src/main/java/com/cloud/study/cqs/eurekaclient/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.cloud.study.cqs.eurekaclient.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class IndexController {
 
     @Value("${server.port}")
@@ -13,8 +15,11 @@ public class IndexController {
     @Value("${spring.application.name}")
     private String applicationName;
 
+    @Value("${test.title}")
+    private String title;
+
     @RequestMapping("index")
     public String index(){
-        return String.format("Hello to %s,port:%d",applicationName,port);
+        return String.format("Hello to %s,port:%d,title=%s",applicationName,port,title);
     }
 }
